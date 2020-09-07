23a652508702da896f2215a0f86ac1332ebc7cbd
bragekankode <bragge@live.no>
2020-09-07 11:15:43 +0200

    test

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    //wasd
 
     @GetMapping(path = "/ping")
     public String ping() {
